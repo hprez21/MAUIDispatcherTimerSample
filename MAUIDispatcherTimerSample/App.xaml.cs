28c3edec3f29d5396da0c1792fd019afbf86fdb5
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2022-11-17 08:59:42 -0600

    Add project files.

---
+++ MAUIDispatcherTimerSample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUIDispatcherTimerSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MAUIDispatcherTimerSample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MAUIDispatcherTimerSample;
+
+public partial class MainPage : ContentPage
+{
+     IDispatcherTimer timer;
+
+     public MainPage()
+     {
+          InitializeComponent();
+     }
+
+     private void Button_Clicked(object sender, EventArgs e)
+     {
+          timer = Dispatcher.CreateTimer();
+          timer.Interval = TimeSpan.FromMilliseconds(1000);
+          timer.Tick += (s, e) =>
+          {
+               Container.SetColumn(MyEllipse, 1);
+               Container.SetRow(MyEllipse, 1);
+          };
+          timer.Start();
+     }
+}
+
